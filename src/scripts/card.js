cd59b763561757310c1fefd44b145eb3d984b662
katwhite <quataridevinni@gmail.com>
2025-03-07 22:30:21 +0300

    реализован попап удаления

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,7 +1,9 @@
 import { deleteCardApi } from "./api";
 import { likeCardApi, unlikeCardApi } from "./api";
+import { openModal, closeModal } from "./modal";
 
 const cardTmp = document.querySelector('#card-template').content;
+const popupDeleteCard = document.querySelector('.popup_type_delete');
 
 export const handleDelete = (card, cardId) => {
   deleteCardApi(cardId).then(() => card.remove());
@@ -23,7 +25,17 @@ export function createCard(userId, {link, name, likes, _id, owner}, onDelete, on
     likeCounter.textContent = likes.length;
     cardElement.querySelector('.card__title').textContent = name;
     if (userId === owner._id) {
-      deleteBtn.addEventListener('click', () => onDelete(cardElement, _id));
+      deleteBtn.addEventListener('click', () => {
+        openModal(popupDeleteCard);
+        const closeDeleteBtn = popupDeleteCard.querySelector('.popup__close');
+        closeDeleteBtn.addEventListener('click', () => closeModal(popupDeleteCard));
+        function submitDeleteCard(evt) {
+          evt.preventDefault(); 
+          onDelete(cardElement, _id);
+          closeModal(popupDeleteCard);
+        }
+        popupDeleteCard.addEventListener('submit', submitDeleteCard);
+      });
     }
     else deleteBtn.setAttribute('disabled', true);
     
